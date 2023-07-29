af9da109248ec3a7767f43b558d6b5439c3800de
Axdev1374 <doktorhous1122@gmail.com>
2023-07-29 22:04:15 +0500

    firstsite

---
+++ main.js
@@ -0,0 +1,59 @@
+
+document.getElementById("tugma").onclick = function(){
+    let savol  = document.getElementById("exampleFormControlTextarea1").value;
+
+
+    let true1 = savol
+        .replace("oqiyman","o'qiyman")
+        .replace("garoyib","g'aroyib")
+        .replace("organaman","o'rganaman")
+        .replace("organmoqchiman","o'rganmoqchiman")
+        .replace("soz berdim","so'z berdim")
+        .replace("mano","ma'no")
+        .replace("kordim","ko'rdim")
+        .replace("korganman","ko'rganman")
+        .replace("oqishni","o'qishni")
+        .replace("oqimasligim","o'qimasligim")
+        .replace("ozimga","o'zimga")
+        .replace("yoq","yo'q")
+        .replace("oqishga","o'qishga")
+        .replace("oqishni","o'qishni")
+        .replace("organishni","o'rganishni")
+        .replace("galaba","g'alaba")
+        .replace("ganimat","g'animat")
+        .replace("oqish","o'qish")
+        // VILOYAT NOMLARI
+        .replace("toshkent","Toshkent")
+        .replace("andijon","Andijon")
+        .replace("samarqand","Samarqand")
+        .replace("jizzax","Jizzax")
+        .replace("qoraqalpogiston","Qo'raqalpog'iston")
+        .replace("nukus","Nukus")
+        .replace("surxandaryo","Surxandaryo")
+        // Ismlar
+        .replace("anvarjon","Anvarjon")
+        .replace("anvar","Anvarjon")
+        .replace("Anvar","Anvarjon")
+        .replace("axmadjon","Axmadjon")
+        .replace("sitora","Sitoraxon")
+        .replace("kamola","Kamolaxon")
+        .replace("nargiza","Nargizaxon")
+        //Istagancha ma'lumot qo'shish mumkin
+
+    document.getElementById("quest1").innerHTML=`Siz yozgan matn :>> ${savol}`;
+    document.getElementById("belgilar").innerHTML=`------------------------------------------------------------------------------`;
+    document.getElementById("quest").innerHTML=`Men to'g'rilagan matn :>> ${true1}`;
+    const sCount = true1.split(' ').length ;
+    document.getElementById("words_count").innerHTML=`Siz ${sCount} ta so'z yozdingiz ishonmasangiz sanab ko'ring`
+
+
+
+}
+
+
+
+
+
+
+
+
